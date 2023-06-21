60151e90d3468a8bb11bb02ed24f38c58a1c39ea
Zalezny <zaleznypl@gmail.com>
2023-06-21 22:15:11 +0200

    added shadow

--- lib/pages/outfit_page/outfit_page_widgets/outfit_item.dart
+++ lib/pages/outfit_page/outfit_page_widgets/outfit_item.dart
@@ -12,60 +12,64 @@ class OutfitItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(12),
-      child: Stack(alignment: Alignment.centerRight, children: [
-        Center(
-          child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                outfit.title!,
-                style: Theme.of(context).textTheme.titleLarge,
-              )),
-        ),
-        showBin
-            ? Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: GestureDetector(
-                  behavior: HitTestBehavior.translucent,
-                  onTap: () {
-                    showDialog(
-                        context: context,
-                        builder: (ctx) {
-                          return AlertDialog(
-                            title: Text(
-                              "Usuń ${outfit.title}",
-                              style: Theme.of(context).textTheme.titleMedium,
-                            ),
-                            content: Text(
-                              "Czy napewno chcesz go usunąć?",
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text("NIE"),
+    return Opacity(
+      opacity: outfit.ended! ? 0.4 : 1.0,
+      child: Card(
+        margin: const EdgeInsets.all(12),
+        child: Stack(alignment: Alignment.centerRight, children: [
+          Center(
+            child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  outfit.title!,
+                  style: Theme.of(context).textTheme.titleLarge,
+                )),
+          ),
+          showBin
+              ? Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.translucent,
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (ctx) {
+                            return AlertDialog(
+                              title: Text(
+                                "Usuń ${outfit.title}",
+                                style: Theme.of(context).textTheme.titleMedium,
                               ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                                child: const Text("TAK"),
-                              )
-                            ],
-                          );
-                        });
-                  },
-                  child: Icon(
-                    Icons.delete_outline,
-                    color: Theme.of(context).colorScheme.secondary,
+                              content: Text(
+                                "Czy napewno chcesz go usunąć?",
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text("NIE"),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    //TODO: remove this item in database and local
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text("TAK"),
+                                )
+                              ],
+                            );
+                          });
+                    },
+                    child: Icon(
+                      Icons.delete_outline,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
                   ),
-                ),
-              )
-            : const SizedBox()
-      ]),
+                )
+              : const SizedBox()
+        ]),
+      ),
     );
   }
 }
