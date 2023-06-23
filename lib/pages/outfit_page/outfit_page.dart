ab5df43c582c5ac5ff0cc69732e3d580af1ad2a1
Zalezny <zaleznypl@gmail.com>
2023-06-23 20:07:36 +0200

    added refresh indicator and change list

--- lib/pages/outfit_page/outfit_page.dart
+++ lib/pages/outfit_page/outfit_page.dart
@@ -5,6 +5,7 @@ import 'package:outfit_flutter/pages/outfit_page/bloc/outfit_bloc.dart';
 import 'package:outfit_flutter/pages/outfit_page/outfit_page_widgets/outfit_item.dart';
 import 'package:outfit_flutter/pages/outfit_page/outfit_page_widgets/outfit_top_row.dart';
 import 'package:outfit_flutter/web_api/connections/outfit_connection.dart';
+import 'package:outfit_flutter/web_api/dto/outfit_dto.dart';
 
 class OutfitPage extends StatefulWidget {
   const OutfitPage({super.key});
@@ -15,6 +16,8 @@ class OutfitPage extends StatefulWidget {
 
 class _OutfitPageState extends State<OutfitPage> {
   bool _showBin = false;
+  final bloc = OutfitBloc(GetIt.I<OutfitConnection>());
+  List<OutfitDto> outfits = [];
 
   void changeShowBin() {
     setState(() {
@@ -32,21 +35,25 @@ class _OutfitPageState extends State<OutfitPage> {
               onIconClicked: changeShowBin,
             ),
             BlocProvider(
-                create: (context) => OutfitBloc(
-                      GetIt.I<OutfitConnection>(),
-                    )..add(SendOutfitEvent()),
+                create: (context) => bloc..add(SendOutfitEvent()),
                 child: BlocBuilder<OutfitBloc, OutfitState>(
                   builder: (context, state) {
                     if (state is OutfitSuccessState) {
+                      outfits = state.model.outfits!.reversed.toList();
                       return Expanded(
-                        child: ListView.builder(
-                            shrinkWrap: true,
-                            reverse: true,
-                            itemCount: state.model.outfits!.length,
-                            itemBuilder: (ctx, index) => OutfitItem(
-                                  outfit: state.model.outfits![index],
-                                  showBin: _showBin,
-                                )),
+                        child: RefreshIndicator(
+                          displacement: 80.0,
+                          onRefresh: () async {
+                            bloc.add(SendOutfitEvent());
+                          },
+                          child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: outfits.length,
+                              itemBuilder: (ctx, index) => OutfitItem(
+                                    outfit: outfits[index],
+                                    showBin: _showBin,
+                                  )),
+                        ),
                       );
                     } else if (state is OutfitFailState) {
                       return const Text("Failed to reload data");
