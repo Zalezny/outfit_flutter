4c1fcd36a3c788c71d680c8077dd136e2572e0ab
Zalezny <zaleznypl@gmail.com>
2023-07-04 19:59:08 +0200

    date picker created

--- lib/pages/stopwatch_page/widgets/stopwatch_bottom_sheet.dart
+++ lib/pages/stopwatch_page/widgets/stopwatch_bottom_sheet.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:intl/intl.dart';
 import 'package:outfit_flutter/theme/app_colors.dart';
 
-class StopwatchBottomSheet extends StatelessWidget {
+class StopwatchBottomSheet extends StatefulWidget {
   const StopwatchBottomSheet({super.key});
+
+  @override
+  State<StopwatchBottomSheet> createState() => _StopwatchBottomSheetState();
+}
+
+class _StopwatchBottomSheetState extends State<StopwatchBottomSheet> {
+  DateTime? _dateTime;
+  Time? _workTime;
+  late TextEditingController _dateTextFormController;
+  late TextEditingController _timeTextFormController;
+
+  @override
+  void initState() {
+    super.initState();
+    _dateTextFormController = TextEditingController(text: _stringifyDateTime(_dateTime));
+    _timeTextFormController = TextEditingController(text: '00:00:00');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +31,8 @@ class StopwatchBottomSheet extends StatelessWidget {
         child: Column(
           children: [
             TextFormField(
+              controller: _dateTextFormController,
               style: Theme.of(context).textTheme.bodyMedium,
-              initialValue: '03/07/2023 21:12',
               readOnly: true,
               decoration: InputDecoration(
                   label: Text(
@@ -24,12 +43,12 @@ class StopwatchBottomSheet extends StatelessWidget {
                     borderSide: BorderSide(color: AppColors.red_1867), // Kolor ramki, gdy pole jest wyłączone
                   ),
                   border: const OutlineInputBorder()),
-              onTap: () {},
+              onTap: () => _selectDateTime(context),
             ),
             const SizedBox(height: 24),
             TextFormField(
+              controller: _timeTextFormController,
               style: Theme.of(context).textTheme.bodyMedium,
-              initialValue: '00:00:00',
               readOnly: true,
               decoration: InputDecoration(
                   label: Text(
@@ -50,5 +69,34 @@ class StopwatchBottomSheet extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _selectDateTime(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2021),
+      lastDate: DateTime.now(),
+    );
+    if (pickedDate != null) {
+      final TimeOfDay? pickedTime = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.now(),
+      );
+      if (pickedTime != null) {
+        final dateTime = pickedDate.add(Duration(hours: pickedTime.hour, minutes: pickedTime.minute));
+        _dateTime = dateTime;
+        _dateTextFormController.text = _stringifyDateTime(_dateTime);
+        setState(() {});
+      }
+    }
+  }
+
+  String _stringifyDateTime(DateTime? dateTime) {
+    return DateFormat('dd/MM/yyyy HH:mm').format(dateTime ?? DateTime.now());
+  }
+
+  void _selectWorkTime(BuildContext context) {
+
   }
 }
